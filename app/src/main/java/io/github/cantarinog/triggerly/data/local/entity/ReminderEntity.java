585e8e2e70aa9adbcbfa1ebc68b483deaec57c44
CantarinoG <guilhermecantarino1709@gmail.com>
2026-04-03 15:02:14 -0300

    feat: add domain model conversion methods to ReminderEntity and TriggerEventEntity

--- app/src/main/java/io/github/cantarinog/triggerly/data/local/entity/ReminderEntity.java
+++ app/src/main/java/io/github/cantarinog/triggerly/data/local/entity/ReminderEntity.java
@@ -1,5 +1,6 @@
 package io.github.cantarinog.triggerly.data.local.entity;
 
+import io.github.cantarinog.triggerly.domain.model.Reminder;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -22,4 +23,34 @@ public class ReminderEntity {
     public String soundUri;
 
     public ReminderEntity() {}
+
+    public static ReminderEntity fromDomain(Reminder reminder) {
+        ReminderEntity entity = new ReminderEntity();
+        entity.id = reminder.id();
+        entity.name = reminder.name();
+        entity.description = reminder.description();
+        entity.iconName = reminder.iconName();
+        entity.colorHex = reminder.colorHex();
+        entity.imageUri = reminder.imageUri();
+        entity.startTime = reminder.startTime().toString();
+        entity.endTime = reminder.endTime().toString();
+        entity.numReminders = reminder.numReminders();
+        entity.soundUri = reminder.soundUri();
+        return entity;
+    }
+
+    public Reminder toDomain() {
+        return new Reminder(
+            id,
+            name,
+            description,
+            iconName,
+            colorHex,
+            imageUri,
+            java.time.LocalTime.parse(startTime),
+            java.time.LocalTime.parse(endTime),
+            numReminders,
+            soundUri
+        );
+    }
 }
--- app/src/main/java/io/github/cantarinog/triggerly/data/local/entity/TriggerEventEntity.java
+++ app/src/main/java/io/github/cantarinog/triggerly/data/local/entity/TriggerEventEntity.java
@@ -1,5 +1,6 @@
 package io.github.cantarinog.triggerly.data.local.entity;
 
+import io.github.cantarinog.triggerly.domain.model.TriggerEvent;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
@@ -25,4 +26,22 @@ public class TriggerEventEntity {
     public boolean isFired;
 
     public TriggerEventEntity() {}
+
+    public static TriggerEventEntity fromDomain(TriggerEvent event) {
+        TriggerEventEntity entity = new TriggerEventEntity();
+        entity.id = event.id();
+        entity.reminderId = event.reminderId();
+        entity.triggerTime = event.triggerTime().toString();
+        entity.isFired = event.isFired();
+        return entity;
+    }
+
+    public TriggerEvent toDomain() {
+        return new TriggerEvent(
+            id,
+            reminderId,
+            java.time.LocalDateTime.parse(triggerTime),
+            isFired
+        );
+    }
 }
