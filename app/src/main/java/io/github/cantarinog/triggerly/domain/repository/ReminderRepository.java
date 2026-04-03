ee468d6c8d74a6bc18905070ad6e73a93076e06e
CantarinoG <guilhermecantarino1709@gmail.com>
2026-04-03 14:34:43 -0300

    chore: add Room persistence library dependencies

--- app/src/main/java/io/github/cantarinog/triggerly/domain/repository/ReminderRepository.java
+++ app/src/main/java/io/github/cantarinog/triggerly/domain/repository/ReminderRepository.java
@@ -2,6 +2,7 @@ package io.github.cantarinog.triggerly.domain.repository;
 
 import java.util.List;
 import io.github.cantarinog.triggerly.domain.model.Reminder;
+import io.github.cantarinog.triggerly.domain.model.TriggerEvent;
 
 public interface ReminderRepository {
     void saveReminder(Reminder reminder);
